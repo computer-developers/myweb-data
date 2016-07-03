cf08a01e2f0bf0c6b363639706d086e504edf64c
unknown <neel.patel.2012.np@gmail.com>
2016-07-03 08:33:20 +0530

    renamed Java_003_1. add content in Java_003_1.

--- Java(003)/code003/Code_003_1.java
+++ Java(003)/code003/Code_003_1.java
@@ -1,12 +1,14 @@
 class Test{
 	public static void main(String[] args){
-		String a[]={"it's Test2"};
+		String a[]=new String[2];
+		a[0]=new String("it's Test2.");
 	    System.out.println("hello\nit's Test");
 		Test2.main(a);
 	}
 }
 class Test2{
 	public static void main(String[] a){
+		if(a==null)return;
 		System.out.println(a[0]);
 	}
 }
