9d5a68a1a7dd22b6ec626d8945136f09d6eab8ba
unknown <neel.patel.2012.np@gmail.com>
2016-07-03 09:17:01 +0530

    modify Code_003_1.java

--- Java(003)/code003/Code_003_1.java
+++ Java(003)/code003/Code_003_1.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(String[] args){
-		String a[]=new String[2];
+		String a[]=new String[1];
 		a[0]=new String("it's Test2.");
 	    System.out.println("hello\nit's Test");
 		Test2.main(a);
@@ -8,7 +8,6 @@ class Test{
 }
 class Test2{
 	public static void main(String[] a){
-		if(a==null)return;
 		System.out.println(a[0]);
 	}
 }
