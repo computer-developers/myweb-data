816f4d8372e995f0ddbd5bd4e2cfa472362480f6
unknown <neel.patel.2012.np@gmail.com>
2016-07-07 11:18:11 +0530

    rename to code002.

--- C++_002/code002/C++_002_2_2.cpp
+++ C++_002/code002/C++_002_2_2.cpp
@@ -1,35 +1,25 @@
-//call by value
 #include <iostream>
 using namespace std;
- 
-// function declaration
-void swap(int x, int y);
- 
+void swap(int &x,int &y);
 int main ()
 {
-   // local variable declaration:
+//local variable declaration:
    int a = 100;
    int b = 200;
- 
    cout << "Before swap, value of a :" << a << endl;
    cout << "Before swap, value of b :" << b << endl;
- 
-   // calling a function to swap the values.
-   swap(a, b);
- 
+//calling a function to swap the values using variable reference.
+   swap(a,b);
+//swapping done on actual a & b, not on copy.
    cout << "After swap, value of a :" << a << endl;
    cout << "After swap, value of b :" << b << endl;
- 
    return 0;
 }
-
-void swap(int x, int y)
+void swap(int &x, int &y)
 {
    int temp;
-
-   temp = x; /* save the value of x */
-   x = y;    /* put y into x */
-   y = temp; /* put x into y */
-  
+   temp = x; // save the value at address x
+   x = y;    // put y into x 
+   y = temp; // put x into y
    return;
 }
