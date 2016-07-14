f107a4a1435cf4bce45c145c20993f047e1de001
Neel Patel <neel.patel.2012.np@gmail.com>
2016-07-14 15:45:58 +0530

    add Code_003_6

---
+++ Java_003/code003/Code_003_6.java
@@ -0,0 +1,9 @@
+class Test{
+	public static void main(String args[]){
+	    System.out.println("hello\nit's main with arguments");
+		main();
+	}
+	public static void main(){
+		System.out.println("it's main without any arguments");
+	}
+}
